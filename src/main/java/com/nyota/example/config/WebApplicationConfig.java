63e18263d6872e2667929c606d819ae29cd153f7
Mukul Kumar <kumarmuk@mail.com>
2020-12-07 11:15:09 +0000

    First Commit - project structure

---
+++ src/main/java/com/nyota/example/config/WebApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.nyota.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebApplicationConfig {
+
+}

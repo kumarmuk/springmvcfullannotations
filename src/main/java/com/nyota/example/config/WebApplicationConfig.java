9f2ac1c8ab9fb2f6cfef74d9c4ff5929b0c7a07d
Mukul Kumar <kumarmuk@mail.com>
2021-01-04 17:45:02 +0000

    JSP View resolver

--- src/main/java/com/nyota/example/config/WebApplicationConfig.java
+++ src/main/java/com/nyota/example/config/WebApplicationConfig.java
@@ -1,10 +1,70 @@
 package com.nyota.example.config;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
+@ComponentScan(basePackages = "com.nyota.example.controller")
 public class WebApplicationConfig {
+
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+
+	// @Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	/**
+	 * Register a locale resolver and set the default locale to anything that is
+	 * valid here us has been set as default.
+	 * 
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		System.out.println(slr.getDefaultTimeZone());
+		slr.setDefaultLocale(Locale.ENGLISH);
+		return slr;
+	}
+
+	/**
+	 * This bean will intercept when the request comes in with the lang parameter
+	 * 
+	 * @return
+	 */
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Bean(name = "messageSource")
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		return messageSource;
+	}
 
 }
